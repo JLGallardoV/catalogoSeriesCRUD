6d8e796aa697ecd4117633c37445a5ac7d2ec807
JLGallardoV <Gallardo.vaca@gmail.com>
2020-06-13 15:22:18 -0500

    REFERENCIANDO LAS CAPAS Y GENERANDO CLASES EN LA CAPA DE DATOS2

--- Capa_Datos/clsUsuario.cs
+++ Capa_Datos/clsUsuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Capa_Datos
 {
     class clsUsuario
     {
+        public void listarSeries()
+        {
+           //listaremos los usuarios 
+
+        }
     }
 }
